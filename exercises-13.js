07709640cccf529af0405644b4a41c5531cbe857
Harfi <harfialpharaby@gmail.com>
2019-10-31 19:05:32 +0700

    ubah logic exercises-13.js

--- exercises-13.js
+++ exercises-13.js
@@ -1,27 +1,20 @@
 function targetTerdekat(arr) {
     // you can only write your code here!
-    var locO, terdekat;
-    var locX = [];
+    var x,o,terdekat;
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === 'o') {
-            locO = i;
+        if (arr[i] === 'x') {
+            x = i;
+        } else if (arr[i] === 'o') {
+            o = i;
         }
-        if(arr[i] === 'x') {
-            locX.push(i);
-        }
-    }
-    
-    if(locO == undefined || locX.length == 0){
-        terdekat =  0;
-    }else{
-        for (let i = 0; i < locX.length; i++) {
-            if(Math.abs(locX[i]-locO)<terdekat || terdekat == undefined) {
-                terdekat = Math.abs(locX[i]-locO);
-            }
+
+        // ubah terdekat JIKA (x DAN o tidak kosong) DAN (terdekat tidak kosong ATAU terdekat lebih besar dari selisih x-o)
+        if((x !== undefined && o !== undefined) && (terdekat === undefined || terdekat > Math.abs(x-o))){
+            terdekat = Math.abs(x-o);
         }
     }
     
-    return terdekat;
+    return terdekat === undefined ? 0 : terdekat;
 }
 
 // TEST CASES
@@ -29,4 +22,4 @@ console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
 console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
 console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
 console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+console.log(targetTerdekat(['x', ' ', 'o', 'x', 'x', ' ', ' ', ' '])); // 2
