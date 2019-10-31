500380764c8b88ca43c5511aa4206d53d88a4c81
Harfi <harfialpharaby@gmail.com>
2019-10-31 19:46:06 +0700

    refactor comment exercises-15

--- exercises-15.js
+++ exercises-15.js
@@ -5,17 +5,17 @@ function groupAnimals(animals) {
     var huruf = []; // array untuk mengklasifikasikan per huruf
     var index; // untuk ambil index huruf per nama hewan
     for (let i = 0; i < animals.length; i++) {
+        
         // cek apakah di array huruf sudah ada huruf pada hewan (huruf_pada_hewan = animals[i][0])?
-        // jika ada = ambil index array huruf itu (huruf.indexOf(huruf_pada_hewan))
-        // jika belum ada = push huruf itu ke array huruf (push_huruf = huruf.push(huruf_pada_hewan))
-        //                  dan ambil indexnya (index = push_huruf-1)
-        huruf.includes(animals[i][0]) ? index = huruf.indexOf(animals[i][0]) : index = huruf.push(animals[i][0])-1;
+        huruf.includes(animals[i][0]) ? 
+            index = huruf.indexOf(animals[i][0]) : // jika ada = ambil index array huruf itu (huruf.indexOf(huruf_pada_hewan))
+            index = huruf.push(animals[i][0])-1; // jika belum ada = push huruf itu ke array huruf (push_huruf = huruf.push(huruf_pada_hewan)) dan ambil indexnya (index = push_huruf-1)
 
         // jika hasilnya di index dengan huruf tertentu masih belum ada maka push dalam bentuk array baru, jika sudah ada maka push di array dalam array nya
         // cek apakah hasil di index yang sesuai dengan index huruf belum ada isinya (hasil[index] == undefined)?
-        // jika belum terisi = push hasil dengan nama hewan (nama_hewan = animals[i]) dalam bentuk array (hasil.push([nama_hewan]))
-        // jika sudah terisi = push array dalam array hasil dengan nama hewan (hasil[index].push(nama_hewan))
-        hasil[index] == undefined ? hasil.push([animals[i]]) : hasil[index].push(animals[i]);
+        hasil[index] == undefined ? 
+            hasil.push([animals[i]]) : // jika belum terisi = push hasil dengan nama hewan (nama_hewan = animals[i]) dalam bentuk array (hasil.push([nama_hewan]))
+            hasil[index].push(animals[i]); // jika sudah terisi = push array dalam array hasil dengan nama hewan (hasil[index].push(nama_hewan))
     }
     return hasil;
 }
